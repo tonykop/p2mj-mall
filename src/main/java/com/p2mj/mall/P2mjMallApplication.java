fa2fa22ba3a865fecc9d0159d99920c9eebc5ce6
tonykop <947822402@qq.com>
2023-07-19 15:34:48 +0800

    first commit

---
+++ src/main/java/com/p2mj/mall/P2mjMallApplication.java
@@ -0,0 +1,13 @@
+package com.p2mj.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2mjMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2mjMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2mj/mall/P2mjMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2mj.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2mjMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
